50790cac72b03700b9899a6c456e356cb7cd9c56
Muja-b <ballasmujahed@gmail.com>
2024-07-27 17:46:30 +0300

    adding shortcuts

---
+++ shortcuts.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <unordered_map>
+#ifndef shortcuts 
+#define shortcuts 
+using namespace std;
+using ll = long long;
+using ui = unsigned int;
+using us = unsigned short;
+using vi = std::vector<int>;
+using pi = std::pair<int,int>;
+using map = std::unordered_map<int,int>;
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+
+
+#endif // shortcuts 
