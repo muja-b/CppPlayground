a6b2414521466d96ccaf60c0a3f14314fa95d336
Muja-b <ballasmujahed@gmail.com>
2024-07-27 16:51:29 +0300

    adding C++ shortcuts

---
+++ shortcuts.cpp
@@ -0,0 +1,17 @@
+#ifndef shortcuts 
+#define shortcuts 
+
+typedef long long ll;
+typedef ui = unsigned int;
+typedef us = unsigned short;
+typedef vector<int>vi;
+typedef pair<int,int>pi;
+typedef map<int,int>pi;
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+
+#endif // shortcuts 
